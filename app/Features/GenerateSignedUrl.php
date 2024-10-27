8a9868b44d8573819f38f168080d138666e4dc72
Marius Beckers <marius@beckers.de>
2024-10-27 12:35:20 +0100

    fix syntax and refactor to feature

--- app/Features/GenerateSignedUrl.php
+++ app/Features/GenerateSignedUrl.php
@@ -4,21 +4,25 @@ namespace App\Features;
 
 use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\Facades\URL;
-use const OPENSSL_ALGO_MD2;
+use Illuminate\Support\Str;
 
 class GenerateSignedUrl
 {
     public function __invoke(string $url, array $params = []): string
     {
         $privateKey = Storage::get('private.key');
+        $params = URL::formatParameters($params);
+        if (!empty($params)) {
+            $url = $url . (Str::contains($url, '?') ? '&' : '?') . http_build_query($params);
+        }
 
         openssl_sign($url, $signature, $privateKey, OPENSSL_ALGO_SHA256);
         $encodedSignature = base64_encode($signature);
 
-        $params = URL::formatParameters(array_merge($params, [
-            'signature' => $encodedSignature
-        ]));
-
-        return $url . '&' . http_build_query($params);
+        $params = URL::formatParameters(['signature' => $encodedSignature]);
+        
+        return $url
+            . (Str::contains($url, '?') ? '&' : '?')
+            . http_build_query($params);
     }
 }
